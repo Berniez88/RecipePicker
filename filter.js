e7b9830ac59b32a75008a7c3dff6aa0dc043cb60
Henry Phan <dragondsp3@gmail.com>
2021-11-26 21:43:45 -0500

    Moved creating filter section to new file

---
+++ filter.js
@@ -0,0 +1,79 @@
+import {createRadioBox} from "./radioBox.js"
+
+/**
+ * Helper Functions
+ */
+ function createLabel(labelText, id) {
+    var cellLabel = document.createElement('label')
+    cellLabel.htmlFor = id
+    cellLabel.innerText = labelText
+    return cellLabel
+}
+
+export function createFilter() {
+    var filterDiv = document.createElement("div")
+    filterDiv.id = "filter"
+
+    /**
+     * Create the filter Button
+     */
+    var filterButton = document.createElement("button")
+    filterButton.innerText = "Filter"
+    filterButton.id = "filterButton"
+    filterButton.onclick = () => {
+        const currTable = document.getElementById("filterTable")
+        currTable.style.display = (currTable.style.display === "none")?"block":"none"
+    }
+
+    /**
+     * Create table to contains the filter
+     */
+    var filterTable = document.createElement("table")
+    filterTable.id = "filterTable"
+    filterTable.style.display = "none"
+
+    var filterHeaders = ["Difficulty", "Time", "Categories"]
+    var difficulty = ["Easy", "Medium", "Hard"]
+    var time = ["5 minutes", "10 minute", "1 hour"] 
+    var categories = ["Breakfast", "Salad", "Lunch", "Dinner"]
+
+    var maxRow = Math.max(difficulty.length, Math.max(time.length, categories.length))
+
+    // Setting up the table headers
+    var headerRow = filterTable.insertRow(0)
+
+    for (var headerIndex = 0; headerIndex < filterHeaders.length; headerIndex++) {
+        var headerCell = document.createElement("TH")
+        headerCell.innerText = filterHeaders[headerIndex]
+        headerRow.appendChild(headerCell)
+    }
+
+    // Adding the filter details to the table
+    for (var index = 0; index < maxRow; index++) {
+        var insertedRow = filterTable.insertRow()
+        var difficultyEntry = insertedRow.insertCell()
+        var timeEntry = insertedRow.insertCell()
+        var categoriesEntry = insertedRow.insertCell()
+
+        if (index < difficulty.length) {
+            difficultyEntry.appendChild(createRadioBox("difficulty"))
+            difficultyEntry.appendChild(createLabel(difficulty[index]), "difficulty")   
+        }
+
+        if (index < time.length) {
+            timeEntry.appendChild(createRadioBox("prepTime"))
+            timeEntry.appendChild(createLabel(time[index]), "prepTime")    
+        }
+        
+        if (index < categories.length) {
+            categoriesEntry.appendChild(createRadioBox("categories"))
+            categoriesEntry.appendChild(createLabel(categories[index], "categories"))
+        }
+        
+    }
+
+    filterDiv.appendChild(filterButton)
+    filterDiv.appendChild(filterTable)
+
+    return filterDiv
+}
