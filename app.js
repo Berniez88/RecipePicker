026468b477206adafe825a6a5500f7dc53400375
Bernie Zhong <berniezhong@gmail.com>
2021-12-04 12:17:51 -0500

    Get data and display it on the HTML

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import { createFilter } from "./filter.js";
+import { addRecipes } from "./file.js";
+var mainBody = document.body;
 
-var mainBody = document.body
+var a = document.createElement("p");
+a.textContent = "After Filter";
 
-var a =document.createElement("p")
-a.textContent = "After Filter"
-
-mainBody.appendChild(createFilter())
-mainBody.appendChild(a)
+mainBody.appendChild(createFilter());
+mainBody.appendChild(a);
+addRecipes();
--- file.js
+++ file.js
@@ -2,7 +2,7 @@
 // console.log(recipes);
 
 //* This Works
-// fetch("./ResturantPickerJSON.json")
+// fetch("./Recipe.json")
 //   .then((recepieData) => {
 //     return recepieData.json();
 //   })
@@ -11,21 +11,46 @@
 //   });
 
 async function loadRecipes() {
-  const response = await fetch("./ResturantPickerJSON.json");
+  const response = await fetch("./Recipe.json");
   const data = await response.json();
   return data;
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-  let recepies = [];
+// document.addEventListener("DOMContentLoaded", async () => {
+//   let recipes = [];
+//   let allData = document.querySelector("#allData");
+//   recipes = await loadRecipes();
+//   for (let i in recipes) {
+//     let pTag = document.createElement("p");
+//     pTag.textContent = i;
+//     console.log(i);
+//     allData.appendChild(pTag);
+//     for (let data in i) {
+//       console.log(data[1]);
+//     }
+//   }
+//   console.log(recipes);
+// });
 
-  let allData = document.querySelector("#allData");
-  allData.appendChild(recepies);
-  try {
-    recepies = await loadRecipes();
-  } catch (e) {
-    console.log("Error");
-    console.log(e);
-  }
-  console.log(recepies);
-});
+//* Working Code
+export function addRecipes() {
+  document.addEventListener("DOMContentLoaded", async () => {
+    let recipes = [];
+    console.log();
+    let allData = document.querySelector("#allData");
+
+    recipes = await loadRecipes();
+    for (var k in recipes) {
+      var temp = "";
+
+      for (var entry in recipes[k]) {
+        temp = temp + recipes[k][entry] + " | ";
+      }
+      var pTag = document.createElement("p");
+      pTag.textContent = temp;
+      allData.appendChild(pTag);
+    }
+
+    console.log(recipes);
+  });
+}
