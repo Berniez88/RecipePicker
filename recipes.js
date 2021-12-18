34713dfb640eca272e2f5630ed2030d729633374
bzhong88 <58304673+Berniez88@users.noreply.github.com>
2021-12-18 12:14:52 -0500

    Setting up recipe class

---
+++ recipes.js
@@ -0,0 +1,16 @@
+const RecipeInput = {
+    Name?,
+    rating?,
+    easeofPrep,
+    type,
+    prepTime,
+    image,
+    ingredients,
+    link
+}
+
+class Recipe extends RecipeInput{
+    constructor(recipeinfo){
+        this.Name
+    }
+}
